f6a4f530c5dbce742b58bba88cb7bee80de688a5
KOSINEPALLI ARJUN SAI <sreesaiarjun@gmail.com>
2025-05-04 01:30:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -208,8 +208,6 @@ except NameError:
 # Sample filenames expected in the script's directory
 sample_pairs = [
     ("blurred_0_000009.png", "sharp_0_000009.png"),
-    ("blurred_0_000005.png", "sharp_0_000005.png"),
-    ("blurred_0_000010.png", "sharp_0_000010.png"),
 ]
 
 # --- Check if files exist relative to the script directory ---
